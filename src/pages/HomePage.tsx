5ac1637dddea5c74abb4071148a140ce622bc79e
MS24909396 <you@example.com>
2025-03-15 20:08:40 +0530

    NF commit``

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -6,20 +6,28 @@ const HomePage: React.FC = () => {
     return (
         <div className="home-page">
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                <a className="navbar-brand" href="/">Test Case Tool</a>
+                <a className="navbar-brand" href="/">Logo</a>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarNav">
-                    <ul className="navbar-nav">
+                    <ul className="navbar-nav mr-auto">
                         <li className="nav-item">
                             <a className="nav-link" href="/">Home</a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="/about">About</a>
+                            <a className="nav-link" href="/test-case-generation">Test Case Generation</a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="/contact">Contact</a>
+                            <a className="nav-link" href="/test-case-templates">Test Case Templates</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="/quality-assurance-knowledge">Quality Assurance Knowledge</a>
+                        </li>
+                    </ul>
+                    <ul className="navbar-nav ml-auto">
+                        <li className="nav-item">
+                            <a className="nav-link" href="/profile">Profile</a>
                         </li>
                     </ul>
                 </div>
